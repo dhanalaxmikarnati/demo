150965481bd01708ae42b3e518fb42b2e65ac554
dhanalaxmikarnati <34591486+dhanalaxmikarnati@users.noreply.github.com>
2023-10-11 11:53:18 +0530

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+          first_name = request.POST['first_name']
+          last_name = request.POST['last_name']
+          username = request.POST['username']
+          email = request.POST['email']
+          password1 = request.POST['password1']
+          password2 = request.POST['password2']   
+          user = User.objects.create_user(username=username,
+                                          first_name=first_name,
+                                          last_name=last_name,
+                                          email=email,
+                                          password=password1)
+          user.save();
+          print('user created')
+          return redirect('/')
+    else:
+        return render(request,'register.html')
+#def login(request):
+   # return render(request,'login.html')
