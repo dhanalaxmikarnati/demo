1fbc7fdb6fe06126cbfea37abb1f12c6e8bb149f
dhanalaxmikarnati <34591486+dhanalaxmikarnati@users.noreply.github.com>
2023-10-11 15:59:15 +0530

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.models import User,auth
 # Create your views here.
 def register(request):
@@ -9,13 +10,22 @@ def register(request):
           email = request.POST['email']
           password1 = request.POST['password1']
           password2 = request.POST['password2']   
-          user = User.objects.create_user(username=username,
-                                          first_name=first_name,
-                                          last_name=last_name,
-                                          email=email,
-                                          password=password1)
-          user.save();
-          print('user created')
+          
+          if(password1==password2):
+             if User.objects.filter(username=username).exists():
+                messages.info(request,'Username already taken')
+                return redirect('register')
+             elif User.objects.filter(email=email).exists():
+                messages.info(request,'email already taken')
+                return redirect('register')
+             else:
+                user = User.objects.create_user(username=username,first_name=first_name,last_name=last_name,email=email,password=password1)
+                user.save();
+                print('user created')
+            
+          else:
+             print("password not matching....")
+             return redirect('register')
           return redirect('/')
     else:
         return render(request,'register.html')
